7d91bcf11e6f4643dac76d419b822b5808cf5cc6
InfinityStoned <70389092+InfinityStoned@users.noreply.github.com>
2022-11-16 00:34:50 -0600

    browser close

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const puppeteer = require('puppeteer');
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('')
+  await page.goto('');
+  
 
+  await browser.close();
 }
 
 run();
