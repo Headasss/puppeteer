ffa03f5d65b7166b1d743bf5842ddc2a97074506
InfinityStoned <70389092+InfinityStoned@users.noreply.github.com>
2023-01-05 23:32:09 -0600

    evaluate

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const puppeteer = require('puppeteer');
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('');
+  await page.goto('https://stockx.com/');
+
+  const data = await page.evaluate(() => {
+    // your JavaScript code here
+    return data;
+  });
+
   
 
   await browser.close();
